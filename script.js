68ff3727515e541b84ed744dc888d7975028b3e2
umarkhan <umarkhandada461@gmail.com>
2025-07-07 16:01:37 +0500

    modification

---
+++ script.js
@@ -0,0 +1,26 @@
+// weather app
+
+const weatherform = document.getElementsByClassName(".weatherform");
+const cityinput = document.getElementsByClassName(".cityinpuy");
+const card = document.getElementsByClassName(".card");
+const apikey = "ec2d61d2ea75d33eb98804910e9713d9";
+weatherform.addEventListener("submit ", event => {
+
+event.prevenDefault();
+const city = cityinput.value;
+
+
+});
+async function getweatherdata(city) {
+
+}
+function displayweather(data) {
+
+}
+function getweatherEmoji(weatherId) {
+
+}
+function displayerror(message) {
+
+}
+
